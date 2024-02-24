7f5f219f1a19e703d83c906a9bbfb502e1e6114f
Documenter.jl <documenter@juliadocs.github.io>
2024-02-24 03:21:22 +0000

    build based on 0e46dcc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ARFF.jl-Documentation","page":"Home","title":"ARFF.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for the adaptive random fourier features package.","category":"page"}]
+}
