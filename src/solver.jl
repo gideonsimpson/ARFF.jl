b673ea22987142562633df2f5f345912547b06d8
Gideon Simpson <gideon.simpson@gmail.com>
2024-10-21 15:54:47 -0400

    annotated ARFFSolver

--- src/solver.jl
+++ src/solver.jl
@@ -1,3 +1,14 @@
+"""
+    ARFFSolver{TS, TE, TR, TI<:Integer, TL}
+
+Data structure containing key parameters for ARFF training
+### Fields
+* `linear_solve!` - User specified solver for the normal equations
+* `mutate!` - In place transformation for the mutation/exploration step
+* `resample!` - In place transformation for the resampling step
+* `n_epochs` - Total number of training epochs
+* `loss` - Loss function
+"""
 struct ARFFSolver{TS, TE, TR, TI<:Integer, TL}
     linear_solve!::TS
     mutate!::TE
